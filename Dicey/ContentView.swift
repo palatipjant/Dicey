f01426808a22aa2563011418baa452ec76b921de
Palatip Jantawong <palatipjant@outlook.com>
2023-12-12 12:26:46 +0700

    Initial Commit

---
+++ Dicey/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dicey
+//
+//  Created by Palatip Jantawong on 12/12/2566 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dicey/DiceyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceyApp.swift
+//  Dicey
+//
+//  Created by Palatip Jantawong on 12/12/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct DiceyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
