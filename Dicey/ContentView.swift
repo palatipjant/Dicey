86ee3741a69e9c66981cc10cb8c6ce5d45cfb75f
Palatip Jantawong <palatipjant@outlook.com>
2023-12-17 23:20:08 +0700

    iniit

--- Dicey/ContentView.swift
+++ Dicey/ContentView.swift
@@ -8,14 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var leftDiceNumber = 1
+    @State var rightDiceNumber = 1
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Dicey")
+                .foregroundStyle(Color.cyan)
+                .bold()
+                .font(.system(size: 50))
+                .fontWeight(.black)
+            
+            Spacer()
+            
+            Button(action: {
+                leftDiceNumber = Int.random(in: 1...6)
+                rightDiceNumber = Int.random(in: 1...6)
+            }, label: {
+                Text("Click me to roll the Dice!")
+            })
+            
+            Spacer()
+            
+            HStack{
+                Image("dice\(leftDiceNumber)")
+                    .resizable()
+                    .frame(width: 200, height: 200)
+                Image("dice\(rightDiceNumber)")
+                    .resizable()
+                    .frame(width: 200, height: 200)
+            }.offset(y: -200)
+                .padding()
         }
-        .padding()
     }
 }
 
